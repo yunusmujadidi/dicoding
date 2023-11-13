53161fa611542c2c6f9e098f2f3bd17cd4909cca
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-13 23:44:54 +0700

    Removed unused files and code

--- bookshelf-apps/app.js
+++ bookshelf-apps/app.js
@@ -1,118 +1,119 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const bookForm = document.getElementById("bookForm");
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
-  const yearInput = document.getElementById("year");
-  const isCompleteInput = document.getElementById("isComplete");
-  const incompleteShelf = document.getElementById("incompleteShelf");
-  const completeShelf = document.getElementById("completeShelf");
+// Mengambil elemen-elemen HTML
+const bookForm = document.getElementById("bookForm");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const yearInput = document.getElementById("year");
+const isCompleteInput = document.getElementById("isComplete");
+const incompleteShelf = document.getElementById("incompleteShelf");
+const completeShelf = document.getElementById("completeShelf");
 
-  bookForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addBook();
-  });
+//Membuat array yang menyimpan data buku
+let books = [];
 
-  function addBook() {
-    const title = titleInput.value;
-    const author = authorInput.value;
-    const year = yearInput.value;
-    const isComplete = isCompleteInput.checked;
+// Menampilkan buku pada rak berdasarkan kondisi isComplete
+function displayBooks() {
+  incompleteShelf.innerHTML = "";
+  completeShelf.innerHTML = "";
 
-    const book = {
-      id: +new Date(),
-      title,
-      author,
-      year,
-      isComplete,
-    };
-
-    const shelf = isComplete ? completeShelf : incompleteShelf;
+  books.forEach((book) => {
     const bookElement = createBookElement(book);
+    if (book.isComplete) {
+      completeShelf.appendChild(bookElement);
+    } else {
+      incompleteShelf.appendChild(bookElement);
+    }
+  });
+}
 
-    shelf.appendChild(bookElement);
-    updateLocalStorage();
+// Menambahkan buku baru
+function addBook(e) {
+  e.preventDefault();
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const year = yearInput.value;
+  const isComplete = isCompleteInput.checked;
+
+  const book = {
+    id: +new Date(),
+    title,
+    author,
+    year,
+    isComplete,
+  };
+
+  books.push(book);
+  displayBooks();
+  saveBooksToLocalStorage();
+  resetForm();
+}
+
+// Menghapus buku dari rak
+function removeBook(id) {
+  books = books.filter((book) => book.id !== id);
+  displayBooks();
+  saveBooksToLocalStorage();
+}
+
+// Memindahkan buku antar rak
+function moveBook(id) {
+  const bookToMove = books.find((book) => book.id === id);
+  bookToMove.isComplete = !bookToMove.isComplete;
+  displayBooks();
+  saveBooksToLocalStorage();
+}
+
+// Menyimpan data buku ke localStorage
+function saveBooksToLocalStorage() {
+  localStorage.setItem("books", JSON.stringify(books));
+}
+
+// Mendapatkan data buku dari localStorage saat halaman dimuat
+function loadBooksFromLocalStorage() {
+  const storedBooks = localStorage.getItem("books");
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+    displayBooks();
   }
+}
 
-  function createBookElement(book) {
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add(
-      "p-2",
-      "mb-2",
-      "rounded",
-      "border",
-      "border-gray-300",
-      "flex",
-      "justify-between",
-      "items-center"
-    );
-    bookDiv.innerHTML = `
-      <div>
-        <h3 class="font-bold">${book.title}</h3>
-        <p><span class="font-bold">Author:</span> ${book.author}</p>
-        <p><span class="font-bold">Year:</span> ${book.year}</p>
-      </div>
-      <div>
-        <button class="p-2 bg-green-500 text-white rounded mr-2" onclick="moveBook(this.parentElement, ${
-          book.isComplete ? "false" : "true"
-        })">Move</button>
-        <button class="p-2 bg-red-500 text-white rounded" onclick="removeBook(this.parentElement)">Remove</button>
-      </div>
-    `;
-    return bookDiv;
-  }
+// Membuat elemen buku
+function createBookElement(book) {
+  const bookElement = document.createElement("div");
+  bookElement.classList.add(
+    "p-2",
+    "mb-2",
+    "rounded",
+    "border",
+    "border-gray-300",
+    "flex",
+    "justify-between",
+    "items-center"
+  );
+  bookElement.innerHTML = `
+    <div>
+      <h3 class="font-bold">${book.title}</h3>
+      <p><span class="font-bold">Author:</span> ${book.author}</p>
+      <p><span class="font-bold">Year:</span> ${book.year}</p>
+    </div>
+    <div>
+      <button class="p-2 bg-green-500 text-white rounded mr-2" onclick="moveBook(${book.id})">Move</button>
+      <button class="p-2 bg-red-500 text-white rounded" onclick="removeBook(${book.id})">Remove</button>
+    </div>
+  `;
+  return bookElement;
+}
 
-  function moveBook(bookElement, isComplete) {
-    const shelf = isComplete ? completeShelf : incompleteShelf;
-    shelf.appendChild(bookElement);
-    updateLocalStorage();
-  }
+// Membersihkan form setelah buku ditambahkan
+function resetForm() {
+  titleInput.value = "";
+  authorInput.value = "";
+  yearInput.value = "";
+  isCompleteInput.checked = false;
+}
 
-  function removeBook(bookElement) {
-    bookElement.remove();
-    updateLocalStorage();
-  }
+// Memuat data buku saat halaman dimuat
+loadBooksFromLocalStorage();
 
-  function updateLocalStorage() {
-    const incompleteBooks = Array.from(
-      incompleteShelf.querySelectorAll("div")
-    ).map((bookElement) => extractBookInfo(bookElement));
-    const completeBooks = Array.from(completeShelf.querySelectorAll("div")).map(
-      (bookElement) => extractBookInfo(bookElement)
-    );
-
-    localStorage.setItem("incompleteBooks", JSON.stringify(incompleteBooks));
-    localStorage.setItem("completeBooks", JSON.stringify(completeBooks));
-  }
-
-  function extractBookInfo(bookElement) {
-    const bookInfo = bookElement.innerText.split("\n");
-    return {
-      id: +bookInfo[0].split(":")[1].trim(),
-      title: bookInfo[1].split(":")[1].trim(),
-      author: bookInfo[2].split(":")[1].trim(),
-      year: bookInfo[3].split(":")[1].trim(),
-      isComplete: bookElement.parentNode.id === "completeShelf",
-    };
-  }
-
-  function displayBooksFromLocalStorage() {
-    const incompleteBooks =
-      JSON.parse(localStorage.getItem("incompleteBooks")) || [];
-    const completeBooks =
-      JSON.parse(localStorage.getItem("completeBooks")) || [];
-
-    incompleteBooks.forEach((book) => {
-      const bookElement = createBookElement(book);
-      const shelf = document.getElementById("incompleteShelf");
-      shelf.appendChild(bookElement);
-    });
-
-    completeBooks.forEach((book) => {
-      const bookElement = createBookElement(book);
-      const shelf = document.getElementById("completeShelf");
-      shelf.appendChild(bookElement);
-    });
-  }
-
-  displayBooksFromLocalStorage();
-});
+// Menambahkan event listener untuk form
+bookForm.addEventListener("submit", addBook);
