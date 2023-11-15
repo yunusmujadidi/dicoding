4492544491a82a0530c3ea1aaaf77e56d1e911fd
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-15 21:43:49 +0700

    Delete unnecessary files and styles

--- personal-notes-starter/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-// import style
-import './styles/style.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>);
